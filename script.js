d106a9f0c0bc2cb062194fb396013c423082f6c8
saurabh27pal <saurabh27pal1999@gmail.com>
2023-06-07 14:14:10 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+function updateURL() {
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    
+    var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+    document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+  }
